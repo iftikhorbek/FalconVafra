a0199822a42b8c8868a131d19bcb6667336cb173
iftikhorbek <iftikhorbekmuminov@gmail.com>
2025-09-30 16:22:44 +0500

    mouse icon centeralized

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -111,14 +111,14 @@ const Hero = () => {
           </div>
 
         </div>
+      </div>
 
-        {/* Scroll Indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 fade-in-up delay-600">
-          <div className="flex flex-col items-center space-y-2">
-            <span className="text-white/60 text-sm">{t.hero.scrollText}</span>
-            <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center">
-              <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-bounce"></div>
-            </div>
+      {/* Scroll Indicator - Centered within hero section */}
+      <div className="absolute bottom-8 left-0 right-0 z-20 fade-in-up delay-600 pointer-events-none">
+        <div className="flex flex-col items-center justify-center space-y-2 mx-auto w-fit">
+          <span className="text-white/60 text-sm text-center whitespace-nowrap">{t.hero.scrollText}</span>
+          <div className="w-6 h-10 border-2 border-white/30 rounded-full flex justify-center items-start">
+            <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-bounce"></div>
           </div>
         </div>
       </div>
